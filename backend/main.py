a71182594d5aba56dede00a926ce3e693194a185
Amar <amarbavi12345@gmail.com>
2025-06-20 01:25:25 +0530

    issues

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI(title="StAI- Stock Prediction API",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001","http://localhost:5173","https://st-ai.vercel.app/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3001","http://localhost:5173","https://st-ai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
